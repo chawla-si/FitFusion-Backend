1635f4790a2ccb4cbb2f290c8843c3039061d073
chawla-si <chawla.si@northeastern.edu>
2023-11-28 15:41:12 -0500

    Add userdefined exception for resource not found

--- src/main/java/com/fitFusion/springbootlibrary/service/FitnessClassService.java
+++ src/main/java/com/fitFusion/springbootlibrary/service/FitnessClassService.java
@@ -2,6 +2,7 @@ package com.fitFusion.springbootlibrary.service;
 
 import com.fitFusion.springbootlibrary.dao.FitnessClassRepository;
 import com.fitFusion.springbootlibrary.entity.FitnessClass;
+import com.fitFusion.springbootlibrary.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
@@ -14,12 +15,14 @@ public class FitnessClassService {
     @Autowired
     private FitnessClassRepository fitnessClassRepository;
 
-    // Method to get sorted fitness classes
     public List<FitnessClass> getSortedFitnessClasses() {
-        // Specify the sorting order (in this case, by class name in ascending order)
         Sort sort = Sort.by(Sort.Direction.ASC, "className");
 
-        // Retrieve sorted fitness classes from the repository
-        return fitnessClassRepository.findAll(sort);
+        List<FitnessClass> fitnessClassesSorted = fitnessClassRepository.findAll(sort);
+
+        if (fitnessClassesSorted.isEmpty()) {
+            throw new ResourceNotFoundException("No fitness classes found.");
+        }
+        return fitnessClassesSorted;
     }
 }
