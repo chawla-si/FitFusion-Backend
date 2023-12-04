003e2131e116993701c5e3ef6ebff45896bc5cab
chawla-si <chawla.si@northeastern.edu>
2023-12-04 09:43:13 -0500

    Fix cors issue

--- src/main/java/com/fitFusion/springbootlibrary/SpringBootLibraryApplication.java
+++ src/main/java/com/fitFusion/springbootlibrary/SpringBootLibraryApplication.java
@@ -2,12 +2,25 @@ package com.fitFusion.springbootlibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringBootLibraryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootLibraryApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+            }
+        };
     }
 
 }
